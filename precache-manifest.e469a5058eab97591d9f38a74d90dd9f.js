98aa57e1b5525bda0f05bda0740efa589b0d1314
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-05-15 19:49:50 -0400

    Updates

---
+++ precache-manifest.e469a5058eab97591d9f38a74d90dd9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f276f5cad04a15a7f154750817ae8900",
+    "url": "/jscalc/index.html"
+  },
+  {
+    "revision": "3c4ab4f1c2766f37b94d",
+    "url": "/jscalc/static/css/main.b428e747.chunk.css"
+  },
+  {
+    "revision": "dbe2780765e9db9e2dbf",
+    "url": "/jscalc/static/js/2.f117694a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jscalc/static/js/2.f117694a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4ab4f1c2766f37b94d",
+    "url": "/jscalc/static/js/main.1822cf9a.chunk.js"
+  },
+  {
+    "revision": "671e9ad0fa9104418ebb",
+    "url": "/jscalc/static/js/runtime-main.34b935f1.js"
+  }
+]);
